d3e73f0c8255a8c0a9872dbce9640ca2841613d1
Nikhil Rajavasireddy <nrajavas@gmail.com>
2021-01-12 10:49:29 -0800

    Add AWS S3 image uploading feature

--- src/assets/send.js
+++ src/assets/send.js
@@ -10,6 +10,11 @@ app.use(cors());
 
 const fs = require("fs");
 
+var AWS = require("aws-sdk");
+var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+var bucketName = '';
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
@@ -17,6 +22,7 @@ app.post('/send-mail', function (req, res) {
   console.log(req.body.email);
   console.log(req.body.attachment);
   //console.log(req.body.image);
+  uploadImage(req.body.image);
   sendMail(req.body.email, req.body.attachment, req.body.image);
   console.log("EMAIL SUCCESSFULLY SENT")
   res.header("Access-Control-Allow-Origin", "*");
@@ -28,6 +34,21 @@ app.post('/send-mail', function (req, res) {
 app.listen(3000, function () {
   console.log('LISTENING on port 3000');
 })
+
+function uploadImage(userImage) {
+  buf = Buffer.from(userImage.replace(/^data:image\/\w+;base64,/, ""),'base64');
+  var params = {
+    Body: buf, 
+    Bucket: bucketName, 
+    Key: "user_upload.jpg",
+    ContentEncoding: 'base64',
+    ContentType: 'image/jpeg'
+   };
+   s3.putObject(params, function(err, data) {
+     if (err) console.log(err, err.stack);
+     else     console.log(data);
+   });
+}
 
 
 function sendMail(emailInput, pathToAttachment, imagePath) { 
